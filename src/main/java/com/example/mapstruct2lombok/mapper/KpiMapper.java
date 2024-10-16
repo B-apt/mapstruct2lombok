6b7975a71524d8ef2c4232781cd89068596f64d8
Baptiste Andrini <48515764+B-apt@users.noreply.github.com>
2024-10-16 15:05:05 +0200

    Working use case of nested mapper calls

--- src/main/java/com/example/mapstruct2lombok/mapper/KpiMapper.java
+++ src/main/java/com/example/mapstruct2lombok/mapper/KpiMapper.java
@@ -2,9 +2,11 @@ package com.example.mapstruct2lombok.mapper;
 
 import com.example.mapstruct2lombok.dto.Kpi;
 import com.example.mapstruct2lombok.dto.KpiDto;
+import com.example.mapstruct2lombok.dto.KpiMeta;
+import com.example.mapstruct2lombok.dto.KpiMetaDto;
 import org.mapstruct.Mapper;
 
-@Mapper(uses = {KpiMetaMapper.class})
+@Mapper/*(uses = {KpiMetaMapper.class})*/
 public interface KpiMapper {
-    Kpi toKpi(KpiDto kpiDto);
+    Kpi toKpi(KpiDto kpiDto, KpiMetaDto kpiMeta);
 }
